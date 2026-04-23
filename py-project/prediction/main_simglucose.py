1256e48f436426cf32ee86adb42abf08fa4e1a5d
ZhangChuanzhi <zaenzhang@outlook.com>
2026-04-23 13:54:28 +0200

    main_simglucose.py

--- py-project/prediction/main_simglucose.py
+++ py-project/prediction/main_simglucose.py
@@ -5,6 +5,8 @@ from simglucose.actuator.pump import InsulinPump
 from simglucose.simulation.scenario import CustomScenario
 from simglucose.controller.basal_bolus_ctrller import BBController
 from datetime import datetime
+from datetime import timedelta
+from collections import deque
 import pandas as pd
 
 start_time = datetime(2026, 1, 1, 0, 0, 0)
@@ -17,34 +19,49 @@ scenario = CustomScenario(start_time=start_time, scenario=meals)
 ctrl     = BBController()
 
 env = T1DSimEnv(patient, sensor, pump, scenario)
-obs, _, done, info = env.reset()
+obs, _, done, info = env.reset() #  Reset the environment
+
 
 records = []
 MAX_STEPS = 480   # Maximum 480 steps = 24 hours
-LOG_EVERY  = 5    # Print every 5 steps (15 minutes)
+LOG_EVERY  = 5    
 step = 0
+
+LOW_GLUCOSE = 70.0  ##### question 2 and 3
+HIGH_GLUCOSE = 180.0
+PREDICT_INTERVAL_MIN = 30.0
+ALERT_EVERY_MIN = 5.0
+SLOPE_LENGTH = 6
+
+recent = deque(maxlen=SLOPE_LENGTH)
+next_alert_time = start_time
+last_low_alert_time = None
+last_high_alert_time = None
 
 while not done and step < MAX_STEPS:
     t = info['time']
     action = ctrl.policy(
         observation=obs,
-        reward=0,
-        done=False,
-        patient_name='adult#001',
-        meal=info.get('meal', 0),
-        t=t
+        patient_name='adult#001', 
+        meal=info.get('meal', 0), #whether eat or not 
+        t=t 
     )
-    obs, reward, done, info = env.step(action)
-    cgm = obs.CGM
-    insulin = float(action.basal) + float(action.bolus)
-    records.append({'time': t, 'CGM': cgm, 'insulin': insulin})
+    # update environment 
+    obs, _, _, info = env.step(action)
+    t = info['time']
+    cgm = obs.CGM 
+    insulin = float(action.basal)
+    time_cgm_pair = (t, float(cgm))
+    recent.append(time_cgm_pair) 
 
-    if step % LOG_EVERY == 0:
-        status = "🔴 Hypoglycemia" if cgm < 70 else ("🟡 Hyperglycemia" if cgm > 180 else "🟢 Normal")
-        print(f"{t.strftime('%H:%M')} | CGM: {cgm:.1f} mg/dL | {status}")
+    slope = None
+    pred = None
+
+    if len(recent) >= 2:
+        time_cgm_pair0 = recent[0]
+        time_cgm_pair1 = recent[-1]
+        dt_min = (time_cgm_pair1[0] - time_cgm_pair0[0]).total_seconds() / 60.0
+        slope = (time_cgm_pair1[1] - time_cgm_pair0[1]) / dt_min if dt_min > 0 else 0.0
+        pred = float(cgm) + slope * PREDICT_INTERVAL_MIN  #### question1
 
     step += 1
-
-df = pd.DataFrame(records)
-df.to_csv('simglucose_result.csv', index=False)
-print(f"\n✅ Total {len(df)} records, saved to simglucose_result.csv")
